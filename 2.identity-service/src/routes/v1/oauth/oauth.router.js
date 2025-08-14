47868a819a2ab8faa24fd6849ce02ac0fbc31dd2
Grace Akpan <opps4lrng@gmail.com>
2025-08-14 02:17:09 -0600

    update to text in oathRouter

--- 2.identity-service/src/routes/v1/oauth/oauth.router.js
+++ 2.identity-service/src/routes/v1/oauth/oauth.router.js
@@ -2,6 +2,6 @@ const express = require('express');
 const oauthController = require('./oauth.controller');
 
 const oauthRouter = express.Router();
-loginRouter.route('/').post(oauthController.oauthLogin);
+oauthRouter.route('/').post(oauthController.oauthLogin);
 
 module.exports = { oauthRouter };
